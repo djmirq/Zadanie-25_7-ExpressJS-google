563401bf1de12df2f7fd334dd4b92bc14ee0a5be
Miro Dresler <djmirq@wp.pl>
2018-10-27 16:09:41 +0200

    initial commit

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  GOOGLE_CLIENT_ID:
+    "795259996385-uu5ubrbkh483d1fccoclo0lnkjcponpa.apps.googleusercontent.com",
+  GOOGLE_CLIENT_SECRET: "_aE4EYvggQG4zeto0R42Mx2-",
+  CALLBACK_URL: "http://localhost:3000/auth/google/callback"
+};
